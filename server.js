dc92e3da59323af1941a541a575a142ac263cdff
andydevlabs <avotiana@mail.hei.school>
2025-03-23 12:43:47 +0300

    build: integrate ejs

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.set("view engine", "ejs")
+
+app.get("/", (req, res) => {
+    res.render("homePage");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+    console.log(`Server listening on port ${port}`);
+});
