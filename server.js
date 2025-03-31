fd5823f3a58b7188a1a8035019078c5b515ae2c6
andydevlabs <avotiana@mail.hei.school>
2025-03-31 17:40:25 +0300

    feat(/register): implement username data validation

--- server.js
+++ server.js
@@ -4,7 +4,12 @@ const port = 3000;
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
-app.use(express.urlencoded({ extended: true}))
+app.use(express.urlencoded({ extended: true }));
+
+app.use((req, res, next) => {
+    res.locals.validationError = [];
+    next();
+});
 
 app.get("/", (req, res) => {
     res.render("homePage");
@@ -15,9 +20,35 @@ app.get("/login", (req, res) => {
 });
 
 app.post("/register", (req, res) => {
-    res.send("Form filled")
-    console.log(req.body);
-})
+    const validationError = [];
+
+    if (typeof req.body.username !== "string") {
+        req.body.username = "";
+    }
+    if (typeof req.body.password !== "string") {
+        req.body.password = "";
+    }
+
+    proccessed_username = req.body.username.trim();
+
+    if (!proccessed_username)
+        validationError.push("You must provide a username");
+
+    if (proccessed_username && proccessed_username.length < 3)
+        validationError.push("Username must be at least 3 characters");
+
+    if (proccessed_username && proccessed_username.length > 12)
+        validationError.push("Username cannot exceed 12 characters");
+
+    if (proccessed_username && !proccessed_username.match(/^[a-zA-Z0-9]+$/))
+        validationError.push("Username can only contain letters and numbers");
+
+    if (validationError.length) {
+        return res.render("homePage", { validationError });
+    } else {
+        res.send("Thank you for feeling the forms");
+    }
+});
 
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`);
