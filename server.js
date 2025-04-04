edff7fa1e65dfb0def7a310d77b97691ba79ee9a
anydevlabs <avotiana@mail.hei.school>
2025-04-04 23:38:38 +0300

    feat: implement POST login behavior

--- server.js
+++ server.js
@@ -17,7 +17,8 @@ app.use(express.urlencoded({ extended: true }));
 
 // global middleware
 app.use((req, res, next) => {
-    res.locals.validationError = [];
+    res.locals.registrationValidationError = [];
+    res.locals.loginValidationError = [];
 
     // verify the jwt cookie
     try {
@@ -37,7 +38,7 @@ app.use((req, res, next) => {
 
 app.get("/", (req, res) => {
     if (req.authenticationToken) {
-        res.render("dashboard-page");
+        return res.render("dashboard-page");
     } else {
         res.render("registration-page");
     }
@@ -51,8 +52,8 @@ app.get("/register", (req, res) => {
     res.render("registration-page");
 });
 
-app.post("/register", async (req, res) => {
-    const validationError = [];
+app.post("/login", async (req, res) => {
+    const loginValidationError = [];
 
     if (typeof req.body.username !== "string") {
         req.body.username = "";
@@ -61,53 +62,38 @@ app.post("/register", async (req, res) => {
         req.body.password = "";
     }
 
-    proccessed_username = req.body.username.trim();
+    logged_username = req.body.username.trim();
 
-    if (!proccessed_username)
-        validationError.push("You must provide a username");
+    if (!logged_username)
+        loginValidationError.push("You must provide a username");
 
-    if (proccessed_username && proccessed_username.length < 3)
-        validationError.push("Username must be at least 3 characters");
+    logged_password = req.body.password;
+    if (!logged_password)
+        loginValidationError.push("You must provide a password");
 
-    if (proccessed_username && proccessed_username.length > 12)
-        validationError.push("Username cannot exceed 12 characters");
+    // check if the username is in the database
+    const getUsername = db.prepare(`SELECT * FROM "user" WHERE username = ?`);
+    const userExistsCheck = getUsername.get(logged_username);
+    if (!userExistsCheck) {
+        loginValidationError.push("Invalid username / password");
+    }
+    // check if the password match is in the database
+    const passwordMatch = bcrypt.compare(
+        logged_password,
+        userExistsCheck.password
+    );
+    if (!passwordMatch) {
+        loginValidationError.push("Invalid username / password");
+    }
 
-    if (proccessed_username && !proccessed_username.match(/^[a-zA-Z0-9]+$/))
-        validationError.push("Username can only contain letters and numbers");
-
-    // Password validation
-    proccessed_password = req.body.password;
-    if (!proccessed_password)
-        validationError.push("You must provide a password");
-
-    if (proccessed_password && proccessed_password.length < 8)
-        validationError.push("Password must be at least 8 characters");
-
-    if (proccessed_password && proccessed_password.length > 64)
-        validationError.push("Password cannot exceed 64 characters");
-
-    if (validationError.length) {
-        return res.render("homePage", { validationError });
+    if (loginValidationError.length) {
+        return res.render("login-page", { loginValidationError });
     } else {
-        // hash the password
-        const salt = await bcrypt.genSalt(10);
-        const hash_password = await bcrypt.hash(proccessed_password, salt);
-
-        // query
-        const insertUser = db.prepare(
-            `INSERT INTO "user"(username, password) VALUES (? , ?)`
-        );
-        const insertedUser = insertUser.run(proccessed_username, hash_password);
-
-        const selectUserId = db.prepare(`
-            SELECT * FROM "user" WHERE id = ?`);
-        const registeredUser = selectUserId.get(insertedUser.lastInsertRowid);
-
         // cookie handling
         const userToken = jwt.sign(
             {
-                id: registeredUser.id,
-                username: registeredUser.username,
+                id: userExistsCheck.id,
+                username: userExistsCheck.username,
             },
             process.env.JWT_SECRET,
             { expiresIn: "24h" }
@@ -119,6 +105,75 @@ app.post("/register", async (req, res) => {
             sameSite: "strict",
             maxAge: 1000 * 60 * 60 * 24,
         });
+
+        res.redirect("/");
+    }
+});
+
+app.post("/register", async (req, res) => {
+    const registrationValidationError = [];
+
+    if (typeof req.body.username !== "string") {
+        req.body.username = "";
+    }
+    if (typeof req.body.password !== "string") {
+        req.body.password = "";
+    }
+
+    proccessed_username = req.body.username.trim();
+
+    if (!proccessed_username)
+        registrationValidationError.push("You must provide a username");
+
+    // check if the username already exists
+    const getUsername = db.prepare(`SELECT * FROM "user" WHERE username = ?`);
+    const usernameUniqueCheck = getUsername.run(proccessed_username);
+    if (usernameUniqueCheck) {
+        registrationValidationError.push("Username already exists");
+    }
+
+    if (proccessed_username && proccessed_username.length < 3)
+        registrationValidationError.push(
+            "Username must be at least 3 characters"
+        );
+
+    if (proccessed_username && proccessed_username.length > 12)
+        registrationValidationError.push(
+            "Username cannot exceed 12 characters"
+        );
+
+    if (proccessed_username && !proccessed_username.match(/^[a-zA-Z0-9]+$/))
+        registrationValidationError.push(
+            "Username can only contain letters and numbers"
+        );
+
+    // Password validation
+    proccessed_password = req.body.password;
+    if (!proccessed_password)
+        registrationValidationError.push("You must provide a password");
+
+    if (proccessed_password && proccessed_password.length < 8)
+        registrationValidationError.push(
+            "Password must be at least 8 characters"
+        );
+
+    if (proccessed_password && proccessed_password.length > 64)
+        registrationValidationError.push(
+            "Password cannot exceed 64 characters"
+        );
+
+    if (registrationValidationError.length) {
+        return res.render("registration-page", { registrationValidationError });
+    } else {
+        // hash the password
+        const salt = await bcrypt.genSalt(10);
+        const hash_password = await bcrypt.hash(proccessed_password, salt);
+
+        // query
+        db.prepare(`INSERT INTO "user"(username, password) VALUES (? , ?)`).run(
+            proccessed_username,
+            hash_password
+        );
 
         res.send("Thank you for filling the forms");
     }
