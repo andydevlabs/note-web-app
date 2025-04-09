5a5489c7dd1d1374a88af9a736a5c39eb3ac0a07
andydevlabs <avotiana@mail.hei.school>
2025-04-09 21:27:25 +0300

    feat: implement post creation logic

--- server.js
+++ server.js
@@ -7,11 +7,9 @@ var Tokens = require("csrf");
 const csrfProtect = require("./middleware/csrfProtection");
 require("dotenv").config();
 
-
 const app = express();
 const port = 5030;
 const tokens = new Tokens();
-
 
 app.use(cookieParser());
 
@@ -23,6 +21,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use((req, res, next) => {
     res.locals.registrationValidationError = [];
     res.locals.loginValidationError = [];
+    res.locals.postCreationValidationError = []; 
 
     // verify the jwt cookie
     try {
@@ -30,7 +29,6 @@ app.use((req, res, next) => {
             req.cookies.authentication,
             process.env.JWT_SECRET
         );
-        console.log(decoded);
         req.authenticationToken = decoded;
     } catch (error) {
         req.authenticationToken = false;
@@ -70,64 +68,8 @@ app.get("/register", (req, res) => {
     res.render("registration-page");
 });
 
-app.post("/login", csrfProtect, async (req, res) => {
-    const loginValidationError = [];
-
-    if (typeof req.body.username !== "string") {
-        req.body.username = "";
-    }
-    if (typeof req.body.password !== "string") {
-        req.body.password = "";
-    }
-
-    const logged_username = req.body.username.trim();
-    const logged_password = req.body.password;
-
-    if (!logged_username)
-        loginValidationError.push("You must provide a username");
-
-    if (!logged_password)
-        loginValidationError.push("You must provide a password");
-
-    const getUsername = db.prepare(`SELECT * FROM "user" WHERE username = ?`);
-    const userExistsCheck = getUsername.get(logged_username);
-
-    if (!userExistsCheck && logged_username.length)
-        loginValidationError.push("Invalid username / password");
-
-    // password checking
-    if (userExistsCheck && logged_password) {
-        const passwordMatch = await bcrypt.compare(
-            logged_password,
-            userExistsCheck.password
-        );
-
-        if (!passwordMatch)
-            loginValidationError.push("Invalid username / password");
-    }
-
-    if (loginValidationError.length) {
-        return res.render("login-page", { loginValidationError });
-    }
-
-    // cookie handling
-    const userToken = jwt.sign(
-        {
-            id: userExistsCheck.id,
-            username: userExistsCheck.username,
-        },
-        process.env.JWT_SECRET,
-        { expiresIn: "24h" }
-    );
-
-    res.cookie("authentication", userToken, {
-        httpOnly: true,
-        secure: false,
-        sameSite: "strict",
-        maxAge: 1000 * 60 * 60 * 24,
-    });
-
-    res.redirect("/");
+app.get("/create-post", (req, res) => {
+    res.render("create-post-page");
 });
 
 app.post("/register", csrfProtect, async (req, res) => {
@@ -199,18 +141,125 @@ app.post("/register", csrfProtect, async (req, res) => {
     }
 });
 
+app.post("/login", csrfProtect, async (req, res) => {
+    const loginValidationError = [];
+
+    if (typeof req.body.username !== "string") {
+        req.body.username = "";
+    }
+    if (typeof req.body.password !== "string") {
+        req.body.password = "";
+    }
+
+    const logged_username = req.body.username.trim();
+    const logged_password = req.body.password;
+
+    if (!logged_username)
+        loginValidationError.push("You must provide a username");
+
+    if (!logged_password)
+        loginValidationError.push("You must provide a password");
+
+    const getUsername = db.prepare(`SELECT * FROM "user" WHERE username = ?`);
+    const userExistsCheck = getUsername.get(logged_username);
+
+    if (!userExistsCheck && logged_username.length)
+        loginValidationError.push("Invalid username / password");
+
+    // password checking
+    if (userExistsCheck && logged_password) {
+        const passwordMatch = await bcrypt.compare(
+            logged_password,
+            userExistsCheck.password
+        );
+
+        if (!passwordMatch)
+            loginValidationError.push("Invalid username / password");
+    }
+
+    if (loginValidationError.length) {
+        return res.render("login-page", { loginValidationError });
+    }
+
+    // cookie handling
+    const userToken = jwt.sign(
+        {
+            id: userExistsCheck.id,
+            username: userExistsCheck.username,
+        },
+        process.env.JWT_SECRET,
+        { expiresIn: "24h" }
+    );
+
+    res.cookie("authentication", userToken, {
+        httpOnly: true,
+        secure: false,
+        sameSite: "strict",
+        maxAge: 1000 * 60 * 60 * 24,
+    });
+
+    res.redirect("/");
+});
+
+app.post("/create-post", (req, res) => {
+    const postCreationValidationError = []
+
+    if (typeof req.body.title !== "string") {
+        req.body.title = "";
+    }
+    if (typeof req.body.content !== "string") {
+        req.body.content = "";
+    }
+
+    const processed_title = req.body.title.trim();
+    const processed_content = req.body.content;
+
+
+    if (!processed_title)
+        postCreationValidationError.push("You must provide a title");
+
+    if (!processed_content)
+        postCreationValidationError.push("Your post is empty");
+
+    if (postCreationValidationError.length) {
+        return res.render("create-post-page", {
+            postCreationValidationError,
+            title: processed_title,
+            content: processed_content,
+        });
+    }
+
+    const author_id = req.authenticationToken.id;
+    const insertPost = db.prepare(`INSERT INTO post (post_title, post_content, author_id) VALUES (?, ?, ?)`);
+    insertPost.run(processed_title, processed_content, author_id);
+
+    res.redirect("/");
+});
+
 app.get("/logout", (req, res) => {
     res.clearCookie("authentication");
     res.redirect("/");
 });
+
+const createTablePost = db.prepare(`
+    CREATE TABLE IF NOT EXISTS post(
+    post_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    post_title STRING NOT NULL,
+    post_content STRING NOT NULL,
+    post_creation_date TIMESTAMP NOT NULL DEFAULT (datetime('now', 'utc')),
+    author_id INTEGER NOT NULL,
+    FOREIGN KEY (author_id) REFERENCES "user"(id))
+    `);
+createTablePost.run()
+
 
 const createTableUser = db.prepare(`
     CREATE TABLE IF NOT EXISTS user(
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     username STRING NOT NULL UNIQUE,
     password STRING NOT NULL)`);
-
 createTableUser.run();
+
 
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`);
